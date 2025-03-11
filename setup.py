04f94c8c311c4e62faa1cac7d00f81a4942c9844
Moicky <77806634+Moicky@users.noreply.github.com>
2025-03-11 20:26:46 +0100

    v0.0.28

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 import codecs
 
-VERSION = "0.0.27"
+VERSION = "0.0.28"
 DESCRIPTION = "Getting indicators based on smart money concepts or ICT"
 
 # read the contents of the README file
